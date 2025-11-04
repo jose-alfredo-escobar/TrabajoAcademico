4af4341b875180419c321d9d3a46ca89c19b01a7
jose-alfredo-escobar <josealfredoescobarzurita1@gmail.com>
2025-11-04 14:32:55 -0400

    Update controlador.py

--- Proyectos/listasPython/controlador.py
+++ Proyectos/listasPython/controlador.py
@@ -1,28 +1,27 @@
-from modelo import ListaTareas
-
-
 class ControladorTareas:
     def __init__(self, modelo, vista):
         self.modelo = modelo
         self.vista = vista
 
     def agregar(self):
-        descripcion = self.vista.entrada.get()
-        if descripcion:
-            self.modelo.agregar_tarea(descripcion)
-            self.vista.entrada.delete(0, 'end')
+        titulo = self.vista.entrada_titulo.get()
+        detalle = self.vista.entrada_detalle.get()
+        if titulo:
+            self.modelo.agregar_tarea(titulo, detalle)
+            self.vista.entrada_titulo.delete(0, 'end')
+            self.vista.entrada_detalle.delete(0, 'end')
             self.vista.actualizar_lista()
 
     def completar(self):
-        seleccion = self.vista.lista.curselection()
+        seleccion = self.vista.lista_titulos.curselection()
         if seleccion:
-            descripcion = self.vista.lista.get(seleccion[0]).split(" [")[0]
-            self.modelo.marcar_como_completada(descripcion)
+            titulo = self.vista.lista_titulos.get(seleccion[0]).split(" [")[0]
+            self.modelo.marcar_como_completada(titulo)
             self.vista.actualizar_lista()
 
     def eliminar(self):
-        seleccion = self.vista.lista.curselection()
+        seleccion = self.vista.lista_titulos.curselection()
         if seleccion:
-            descripcion = self.vista.lista.get(seleccion[0]).split(" [")[0]
-            self.modelo.eliminar_tarea(descripcion)
+            titulo = self.vista.lista_titulos.get(seleccion[0]).split(" [")[0]
+            self.modelo.eliminar_tarea(titulo)
             self.vista.actualizar_lista()
